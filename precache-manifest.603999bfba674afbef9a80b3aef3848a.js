de42cfe284c4f19a165f342649467bce24e0fba0
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2020-11-11 19:45:54 +0200

    Updates

---
+++ precache-manifest.603999bfba674afbef9a80b3aef3848a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd846b9c9f8804cb0e86a21fe3514e94",
+    "url": "/Map-Markers-App/index.html"
+  },
+  {
+    "revision": "9a2259a32ac8694bebef",
+    "url": "/Map-Markers-App/static/css/main.812da40b.chunk.css"
+  },
+  {
+    "revision": "fe5891aaef2592f7c1fc",
+    "url": "/Map-Markers-App/static/js/2.41bfffb6.chunk.js"
+  },
+  {
+    "revision": "b4a3f1cde4f943012bfe501ccd7ed8f2",
+    "url": "/Map-Markers-App/static/js/2.41bfffb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a2259a32ac8694bebef",
+    "url": "/Map-Markers-App/static/js/main.029dbac0.chunk.js"
+  },
+  {
+    "revision": "a019ff49d36a9e9fed3e",
+    "url": "/Map-Markers-App/static/js/runtime-main.63b4e24d.js"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/Map-Markers-App/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Map-Markers-App/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "7802c6aa236ce8e242303ab9d30f98fc",
+    "url": "/Map-Markers-App/static/media/logo.7802c6aa.png"
+  },
+  {
+    "revision": "36e27b4df80e0a89de70c88fb32126a2",
+    "url": "/Map-Markers-App/static/media/profile.36e27b4d.jpg"
+  }
+]);
